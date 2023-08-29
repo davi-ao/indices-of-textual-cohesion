096cf9b110d8355aabcc92461ffc94292cd9ee6c
Davi Alves Oliveira <davi.alvesoliveira@gmail.com>
2023-08-29 20:14:17 +0000

    Synonyms and Hypernyms from Wordnet

--- code/get_synonyms_and_hypernyms.R
+++ code/get_synonyms_and_hypernyms.R
@@ -1,6 +1,5 @@
 # Load required packages
 library(tidyverse)
-library(rJava)
 library(wordnet)
 
 # Set Wordnet English dictionary
@@ -10,21 +9,45 @@ setDict('data/wn3.1.dict/dict/')
 lemmas = corpus %>%
   distinct(lemma, pos)
 
-synonyms_hypermyns = mapply(function(lemma, pos) {
-  print(lemma)
+synonyms_hypernyms = mapply(function(lemma, pos) {
+  pos_wordnet = NA
+  
   if(pos %in% c('VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ')) {
-    synonyms(lemma, 'VERB') %>%
-      paste(collapse = '|')
+    pos_wordnet = 'VERB'
   } else if (pos %in% c('NN', 'NNS', 'NNP', 'NNPS')) {
-    synonyms(lemma, 'NOUN') %>%
-      paste(collapse = '|')
+    pos_wordnet = 'NOUN'
   } else if (pos %in% c('ADJ', 'JJ', 'JJR', 'JJS')) {
-    synonyms(lemma, 'ADJECTIVE') %>%
-      paste(collapse = '|')
+    pos_wordnet = 'ADJECTIVE'
   } else if (pos %in% c('ADV', 'RB', 'RBR', 'RBS')) {
-    synonyms(lemma, 'ADVERB') %>%
-      paste(collapse = '|')
-  } else {
-    NA
+    pos_wordnet = 'ADVERB'
   }
-}, lemmas$lemma[1:2], lemmas$pos[1:2])
+  
+  if (is.na(pos_wordnet)) {
+    return(NA)
+  } else {
+    tibble(lemma = lemma,
+     pos = pos_wordnet,
+     synonyyms = synonyms(lemma, pos_wordnet) %>%
+       paste(collapse = '|'),
+     hypernyms = tryCatch({
+       filter = getTermFilter('ExactMatchFilter', lemma, T)
+       terms = getIndexTerms(pos_wordnet, 1, filter)
+       synsets = getSynsets(terms[[1]])
+       
+       sapply(synsets, function(s) {
+         relatedSynsets = getRelatedSynsets(s, '@')
+         
+         sapply(relatedSynsets, getWord)
+       }) %>%
+         unlist() %>%
+         unique() %>%
+         paste0(collapse = '|')
+     }, error = function(e) {
+       return(NA)
+     }))
+  }
+}, lemmas$lemma, lemmas$pos, SIMPLIFY = F) %>%
+  bind_rows()
+
+# Write lemmas with synonyms and hypernyms
+write_csv(pseudotexts, 'data/synonyms_hypernyms.csv')
